20a87018ef91ea58636b4c2d2c7775d08b24b5f5
kgrac <kasandragraciano05@gmail.com>
2023-07-10 10:47:55 -0500

    first initial commit

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -23,24 +23,28 @@ public class PositionType {
     @Override
     public String toString() { return value; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id && Objects.equals(value, that.value);
-    }
+
+
+
+// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    //  their id fields match.
+@Override
+public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    PositionType that = (PositionType) o;
+    return id == that.id && Objects.equals(value, that.value);
+}
+
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
 
-// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-
     // Getters and Setters:
+
+
 
     public int getId() {
         return id;
